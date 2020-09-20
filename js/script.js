2229cd06faacb985ef58899435ed3ca946c611fd
Cairn MacGregor <cmacg2@hotmail.co.uk>
2020-09-20 14:44:34 +0100

    Commented out unused code

--- js/script.js
+++ js/script.js
@@ -2,15 +2,15 @@
 
 const toggleButton = document.getElementsByClassName("toggle-button")[0]
 const navbarLinks = document.getElementsByClassName("navbar-links")[0]
-const navSearch = document.getElementById("nav_search") 
+// const navSearch = document.getElementById("nav_search") 
 
 
-//Navbar search :((
-navSearch.addEventListener("click", () => {
-    console.log("Clicked")
-    alert("This functionality has not yet been added")
-}
-);
+// //Navbar search :((
+// navSearch.addEventListener("click", () => {
+//     console.log("Clicked")
+//     alert("This functionality has not yet been added")
+// }
+// );
 
 //Navbar toggle
 toggleButton.addEventListener("click", () => {
