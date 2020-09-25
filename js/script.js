b93933883abf54ceba0b9ce2f512bc83f07af541
Cairn MacGregor <cmacg2@hotmail.co.uk>
2020-09-25 22:16:49 +0100

    Added particles to other pages

--- js/script.js
+++ js/script.js
@@ -17,8 +17,3 @@ toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle('active')
 });
 
-
-// /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-// particlesJS.load('particles-js', 'particlesjs-config.json', function() {
-//     console.log('callback - particles.js config loaded');
-//   });
