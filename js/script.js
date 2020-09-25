4b0268826e6567fbeb1a5f954db2f9bc0cb9e469
Cairn MacGregor <cmacg2@hotmail.co.uk>
2020-09-25 13:29:07 +0100

    Added particle JS background

--- js/script.js
+++ js/script.js
@@ -16,3 +16,9 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0]
 toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle('active')
 });
+
+
+// /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+// particlesJS.load('particles-js', 'particlesjs-config.json', function() {
+//     console.log('callback - particles.js config loaded');
+//   });
