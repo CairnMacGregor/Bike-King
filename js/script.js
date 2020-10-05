6c337ee86286e42e2c779ddacd2e1aab6b21de25
Cairn MacGregor <cmacg2@hotmail.co.uk>
2020-10-05 18:20:22 +0100

    Added content to accessories page

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // Get elements for the Navbar Functionality
 
-const toggleButton = document.getElementsByClassName("toggle-button")[0]
-const navbarLinks = document.getElementsByClassName("navbar-links")[0]
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const content = document.getElementsByClassName("test-div")
 // const navSearch = document.getElementById("nav_search") 
 
 
@@ -12,6 +13,33 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0]
 // }
 // );
 
+// Content test
+
+let b1 = document.getElementById("toggle-1");
+let b2 = document.getElementById("toggle-2");
+let b3 = document.getElementById("toggle-3");
+
+
+b1.addEventListener("click", () =>{
+    console.log("Click")
+    content[0].classList.add('show')
+    content[1].classList.remove('show')
+    content[2].classList.remove('show')
+});
+
+b2.addEventListener("click", () =>{
+    console.log("Click")
+    content[0].classList.remove('show')
+    content[1].classList.add('show')
+    content[2].classList.remove('show')
+});
+
+b3.addEventListener("click", () =>{
+    console.log("Click")
+    content[0].classList.remove('show')
+    content[1].classList.remove('show')
+    content[2].classList.add('show')
+});
 //Navbar toggle
 toggleButton.addEventListener("click", () => {
     navbarLinks.classList.toggle('active')
